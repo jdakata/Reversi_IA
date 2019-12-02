38aa7513afdcda900da90afcb34babdb18a8d8b7
Emmanoe DELAR <26841656+emmanoe@users.noreply.github.com>
2019-12-02 15:21:42 +0100

    Start myplayer implementation

--- playerInterface.py
+++ playerInterface.py
@@ -1,7 +1,7 @@
 class PlayerInterface():
     # Returns your player name, as to be displayed during the game
     def getPlayerName(self):
-        return "Not Defined"
+        return "PlayerPlayer"
 
     # Returns your move. The move must be a couple of two integers,
     # Which are the coordinates of where you want to put your piece
@@ -15,16 +15,22 @@ class PlayerInterface():
     # Inform you that the oponent has played this move. You must play it
     # with no search (just update your local variables to take it into account)
     def playOpponentMove(self, x,y):
-        pass
+        assert(self._board.is_valid_move(self._opponent, x, y))
+        print("Opponent played ", (x,y))
+        self._board.push([self._opponent, x, y])
 
     # Starts a new game, and give you your color.
     # As defined in Reversi.py : color=1 for BLACK, and color=2 for WHITE
     def newGame(self, color):
-        pass
+        self._mycolor = color
+        self._opponent = 1 if color == 2 else 2
 
     # You can get a feedback on the winner
     # This function gives you the color of the winner
     def endGame(self, color):
-        pass
+        if self._mycolor == winner:
+            print("I won!!!")
+        else:
+            print("I lost :(!!")
 
 
