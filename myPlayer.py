ec5344eb50e5b8b0c8dbca32ca1aa0d83d0a8a39
Jean-David <akata.jda@gmail.com>
2019-12-18 16:17:59 +0100

    class node + carte de poids

--- myPlayer.py
+++ myPlayer.py
@@ -2,8 +2,39 @@
 
 import time
 import Reversi
+import numpy
 from random import randint
 from playerInterface import *
+
+
+class node:
+    def __init__(self, NodeFather, NodeChildren, NodeBoard, NodeDepth):
+        self._NodeFather = NodeFather
+        self._NodeChildren = NodeChildren
+        self._NodeBoard = NodeBoard
+        self._NodeDepth = NodeDepth
+
+    def isLeaf(self):
+        return self._NodeChildren == []
+
+    def getNodeBoard(self):
+        return self._NodeBoard
+
+
+WeightMap = numpy.array([[500, -25, 10, 5, 4, 4, 5, 10, -25, 500],
+                         [-25, -45,  1, 1, 1, 1, 1,  1, -45, -25],
+                         [ 10,   1,  3, 2, 1, 1, 2,  3,   1,  10],
+                         [  5,   1,  2, 1, 1, 1, 1,  2,   1,   5],
+                         [  5,   1,  2, 1, 1, 1, 1,  2,   1,   5],
+                         [  5,   1,  2, 1, 1, 1, 1,  2,   1,   5],
+                         [  5,   1,  2, 1, 1, 1, 1,  2,   1,   5],
+                         [ 10,   1,  3, 2, 1, 1, 2,  3,   1,  10],
+                         [-25, -45,  1, 1, 1, 1, 1,  1, -45, -25],
+                         [500, -25, 10, 5, 4, 4, 5, 10, -25, 500]])
+
+#def generatetree(root):
+   # root.get
+
 
 class myPlayer(PlayerInterface):
 
@@ -12,25 +43,25 @@ class myPlayer(PlayerInterface):
         self._mycolor = None
 
     def getPlayerName(self):
-        return "Random Player"
+        return "my Player"
 
     def getPlayerMove(self):
         if self._board.is_game_over():
             print("Referee told me to play but the game is over!")
-            return (-1,-1)
+            return (-1, -1)
         moves = [m for m in self._board.legal_moves()]
-        move = moves[randint(0,len(moves)-1)]
+        move = moves[randint(0, len(moves) - 1)]
         self._board.push(move)
         print("I am playing ", move)
-        (c,x,y) = move
-        assert(c==self._mycolor)
+        (c, x, y) = move
+        assert (c == self._mycolor)
         print("My current board :")
         print(self._board)
-        return (x,y) 
+        return (x, y)
 
-    def playOpponentMove(self, x,y):
-        assert(self._board.is_valid_move(self._opponent, x, y))
-        print("Opponent played ", (x,y))
+    def playOpponentMove(self, x, y):
+        assert (self._board.is_valid_move(self._opponent, x, y))
+        print("Opponent played ", (x, y))
         self._board.push([self._opponent, x, y])
 
     def newGame(self, color):
@@ -42,6 +73,3 @@ class myPlayer(PlayerInterface):
             print("I won!!!")
         else:
             print("I lost :(!!")
-
-
-
